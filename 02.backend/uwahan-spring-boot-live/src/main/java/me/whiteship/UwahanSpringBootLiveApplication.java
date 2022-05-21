5bba0451d000849eade2a181fe69d084cf2b0d8b
uyggnodkrap <uyggnodkrap26@gmail.com>
2022-05-21 17:40:09 +0900

    FEAT: setup spring boot

---
+++ 02.backend/uwahan-spring-boot-live/src/main/java/me/whiteship/UwahanSpringBootLiveApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwahanSpringBootLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UwahanSpringBootLiveApplication.class, args);
+	}
+
+}
